bbb364930a2f39531541ba4976ab15df7587a91c
Yaroslavom <yarykomm@gmail.com>
2021-07-14 22:55:28 +0300

    Redux Components

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -4,7 +4,7 @@ import ErrorImg from "../../assets/image/404.png"
 const ErrorIndicator = () => {
   return (
     <div className="error-boundry">
-      <img src={ErrorImg} alt="404-image" />
+      <img src={ErrorImg} alt="404" />
       <h2>Sorry we can't find that page!</h2>
     </div>
   );
